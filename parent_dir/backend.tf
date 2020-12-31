2bfaf7d82c7dff237154b5cf01c1f37e02b97390
Ubuntu <ubuntu@ip-10-32-0-72.us-west-2.compute.internal>
2020-12-31 17:24:29 +0000

    initial commit setup

---
+++ parent_dir/backend.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "0.13.4"
+
+  backend "s3" {
+    region = "us-west-2"
+    bucket = "mm-tf-cloud-giggles-test"
+    key    = "parent_dir/terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+  version = "3.22.0"
+}
