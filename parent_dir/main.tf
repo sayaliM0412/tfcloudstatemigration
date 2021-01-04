b7aa5846da79d229d00fc08f7d2b900aa9124998
Sayali Joshi <sayali.joshi@magicmemories.com>
2021-01-04 22:33:04 +0000

    changing the name of the bucket

--- parent_dir/main.tf
+++ parent_dir/main.tf
@@ -11,7 +11,7 @@ terraform {
 }
 
 resource "aws_s3_bucket" "terraform_s3_bucket_fun_parent_dir" {
-  bucket = "${terraform.workspace}-giggles"
+  bucket = "${terraform.workspace}-giggle"
 }
 output "bucket_environment" {
   value = "${terraform.workspace}-lkasjdlkashjlklife"
