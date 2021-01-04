f96d24ac6ec737dd4993251b9d6999afd7e5041c
Sayali Joshi <sayali.joshi@magicmemories.com>
2021-01-04 17:53:33 +0000

    output added for bucket_environment

--- parent_dir/main.tf
+++ parent_dir/main.tf
@@ -1,15 +1,6 @@
-# Terraform configuration
 
-variable "name_length" {
-  description = "The number of words in the pet name"
-  default     = "3"
+
+output "bucket_environment" {
+  value = terraform.workspace
 }
 
-resource "random_pet" "pet_name" {
-  length    = var.name_length
-  separator = "-"
-}
-
-output "pet_name" {
-  value = random_pet.pet_name.id
-}
