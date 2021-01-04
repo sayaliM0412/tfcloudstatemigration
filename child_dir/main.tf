6e9be3a7174799495ccda54392c9b9981d0cce4f
Sayali Joshi <sayali.joshi@magicmemories.com>
2021-01-04 22:19:16 +0000

    changes made to trigger tf plan

--- child_dir/main.tf
+++ child_dir/main.tf
@@ -1,3 +1,15 @@
+# Terraform configuration
+terraform {
+  backend "remote" {
+    hostname      = "app.terraform.io"
+    organization  = "TrialRun"
+
+    workspaces {
+      prefix = "child-tfcloudstatemigration"
+    }
+  }
+}
+
 resource "aws_s3_bucket" "terraform_s3_bucket_fun" {
   bucket = "${data.terraform_remote_state.parent_dir.outputs.bucket_environment}-giggles-child-dir"
 }
--- parent_dir/main.tf
+++ parent_dir/main.tf
@@ -1,9 +1,18 @@
+# Terraform configuration
+terraform {
+  backend "remote" {
+    hostname      = "app.terraform.io"
+    organization  = "TrialRun"
 
-
-output "bucket_environment" {
-  value = "${terraform.workspace}-lkasjdlkashjlkdahkjdf"
+    workspaces {
+      prefix = "parent-tfcloudstatemigration"
+    }
+  }
 }
 
 resource "aws_s3_bucket" "terraform_s3_bucket_fun_parent_dir" {
   bucket = "${terraform.workspace}-giggles"
+}
+output "bucket_environment" {
+  value = "${terraform.workspace}-lkasjdlkashjlklife"
 }
