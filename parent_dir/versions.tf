adaa8809a823215245c184b0e36df33745b33de9
Sayali Joshi <sayali.joshi@magicmemories.com>
2021-01-05 04:51:14 +0000

    added version

--- parent_dir/versions.tf
+++ parent_dir/versions.tf
@@ -1,10 +1,12 @@
-
 terraform {
   required_version = "0.13.4"
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "3.22.0"
-    }
-  }
 }
+
+variable "region" {
+}
+
+provider "aws" {
+  region  = var.region
+  version = "3.22.0"
+}
+
